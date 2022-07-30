2a3614d5a2d7e90d5dd03f8702989e0ae1562867
AmmarMasood <ammar.masood98@gmail.com>
2022-07-30 15:08:16 +0500

    m

--- Db.js
+++ Db.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const connectDB = async () => {
   const uri =
-    "mongodb+srv://admin:admin@cluster0.kl3go.mongodb.net/?retryWrites=true&w=majority";
+    "mongodb+srv://admin:w5rv4la1XQkAXOEW@cluster0.kl3go.mongodb.net/?retryWrites=true&w=majority";
   const conn = await mongoose.connect(process.env.MONGOOSE_URI || uri, {
     useNewUrlParser: true,
   });
